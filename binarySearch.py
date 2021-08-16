efd398c8fd234de0eab81c86dd976c4948bbfabf
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2021-08-16 08:54:39 +0000

    modified binarySearch

--- binarySearch.py
+++ binarySearch.py
@@ -1,17 +1,18 @@
 def binarySearch(array, firstIndex, lastIndex, x):
     mid = (firstIndex + lastIndex) // 2
-    if array[mid] == x:
-        return mid
-    elif array[mid] > x:
-        return binarySearch(array, firstIndex, mid - 1, x)
-    elif array[mid] < x:
-        return binarySearch(array, mid + 1, lastIndex, x)
+    if lastIndex >= firstIndex:
+        if array[mid] == x:
+            return mid
+        elif array[mid] > x:
+            return binarySearch(array, firstIndex, mid - 1, x)
+        elif array[mid] < x:
+            return binarySearch(array, mid + 1, lastIndex, x)
     else:
         return -1
 
 
 arr = [2, 3, 4, 10, 40]
-x_ = 40
+x_ = 10
 
 # Function call
 result = binarySearch(arr, 0, len(arr) - 1, x_)
